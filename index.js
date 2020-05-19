b7dcdd9c1ecc8836daf0c98b9568dae2df7ed5f4
Nicolas Heinig <nicolas.heinig@gmail.com>
2020-05-19 18:35:33 +0200

    Configure port via env var

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const port = process.env.PORT || 3000;
 
-server.listen(80);
+server.listen(port, () => console.log(`Listening on ${port}`));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/dashboard.html");
